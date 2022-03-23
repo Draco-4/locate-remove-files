97ee5e4b61c800ac0e25144af9e232a2859d0620
Draco-4 <alfredahize@gmail.com>
2022-03-23 11:06:44 -0400

    Added create-mals.sh file

---
+++ create-mals.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+#mkdir -p malDir troDir virDir worDir
+
+cd ../malDir
+touch  malware1.mal malware2.mal trojan3.tro virus4.vir worm5.wor desc1.txt
+cd ../troDir
+touch trojan1.tro trojan2.tro virus3.vir worm4.wor malware5.mal desc2.txt
+cd ../virDir
+touch virus1.vir virus2.vir worm3.wor malware4.mal trojan5.tro desc3.txt
+cd ../worDir
+touch worm1.wor worm2.wor malware3.mal trojan4.tro virus5.vir desc4.txt
+
+
